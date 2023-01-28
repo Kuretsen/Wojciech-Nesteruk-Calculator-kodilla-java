c9008062a93c9639634cf212c13d4c9b13933e2a
Wojciech Nesteruk <wojciech.nesteruk@gmail.com>
2023-01-28 23:28:05 +0100

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,13 @@
 package com.kodilla;
 
 public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        calculator.addAToB(12, 35);
+    }
 }
