bc76368da963895ae5b36a6ad271a49b82ab829e
Wojciech Nesteruk <wojciech.nesteruk@gmail.com>
2023-01-28 23:59:55 +0100

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -5,9 +5,16 @@ public class Calculator {
     public int addAToB(int a, int b) {
         return a+b;
     }
+    public int subtractAFromB(int a, int b) {
+        return b-a;
+    }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        calculator.addAToB(12, 35);
+        int a=16;
+        int b=31;
+
+        System.out.println(a + " + " + b + " = " +calculator.addAToB(a, b));
+        System.out.println(a + " - " + b + " = " +calculator.subtractAFromB(a, b));
     }
 }
